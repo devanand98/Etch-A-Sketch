fd68d0fde8b1e65a20f8b9d1f7c4efe5b32c2fbf
Dev <devanandv12@gmail.com>
2022-04-09 20:25:14 +0530

    Added hover color change to cells

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 const container=document.getElementById('container');
 
+//Make cell grid
 function makeRows(rows,columns){
     container.style.setProperty('--grid-rows',rows);
     container.style.setProperty('--grid-columns',columns);
     for (i=0;i<(rows*columns);i++) {
         let cell = document.createElement('div');
-        cell.innerText=(i+1);
         console.log('--grid-rows');
-        container.appendChild(cell).className="cell";
+        container.appendChild(cell).className="grid-item";
+        cell.addEventListener('mouseover',()=> {
+            cell.style.backgroundColor = "black";
+            });
+  
     }
-
-
 }
 
+
+
+
 makeRows(16,16);
+
+
+
+
+
+
+
+/*const box=document.getElementsByClassName("cell");
+
+    for (i=0;i<(box.length);i++){
+        box[i].addEventListener('mouseover',()=> {
+            box[i].style.backgroundColor = "red";
+            })
+        box[i].addEventListener('mouseout', ()=>{
+            box[i].setAttribute('style','background-color:white;');
+            })
+    }*/
